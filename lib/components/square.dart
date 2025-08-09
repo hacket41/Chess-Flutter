3a79f31241c31579ed4f85158ffc89ae53eddf58
Abhijit Singh <abhijit.singh.0106@gmail.com>
2025-08-09 12:50:18 +0545

    Flutter:ChessApp '2D List for pawn placement; successful'

--- lib/components/square.dart
+++ lib/components/square.dart
@@ -1,13 +1,27 @@
+import 'package:chess_app/components/piece.dart';
+import 'package:chess_app/values/colors.dart';
 import 'package:flutter/material.dart';
 
 class Square extends StatelessWidget {
   final bool isWhite;
-  const Square({super.key, required this.isWhite});
+  final ChessPiece? piece;
+
+  const Square({
+    super.key,
+    required this.isWhite,
+    required this.piece
+  });
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: isWhite ? Colors.grey[200] : Colors.grey[500],
+      color: isWhite ? foregroundColor : backgroundColor,
+      child: piece != null
+          ? Image.asset(
+          piece!.imagePath,
+          color: piece!.isWhite ? Colors.white : Colors.black,
+      ) : null,
     );
   }
-}
+  }
--- lib/game_board.dart
+++ lib/game_board.dart
@@ -1,4 +1,7 @@
+import 'package:chess_app/components/piece.dart';
 import 'package:chess_app/components/square.dart';
+import 'package:chess_app/helper/helper_methods.dart';
+import 'package:chess_app/values/colors.dart';
 import 'package:flutter/material.dart';
 
 class GameBoard extends StatefulWidget {
@@ -9,21 +12,74 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
+
+  // A 2D list for teh chessboard,
+  //each index its a piece
+
+  late List<List<ChessPiece?>> board;
+
+  @override
+  void initState(){
+    super.initState();
+    _initializeBoard();
+  }
+
+  //initialize board
+  void _initializeBoard(){
+    //initialize board with nullsm each piece in coorect position
+
+    List<List<ChessPiece?>> newBoard =
+        List.generate(8, (index) => List.generate(8, (index) => null));
+
+    //Pawn positions
+    for(int i = 0; i< 8; i++){
+      newBoard[1][i] = ChessPiece(
+          type: ChessPieceType.pawn,
+          isWhite: true,
+          imagePath: 'lib/images/pawn.png'
+      );
+
+      newBoard[6][i] = ChessPiece(
+          type: ChessPieceType.pawn,
+          isWhite: false,
+          imagePath: 'lib/images/pawn.png'
+      );
+    }
+    //Rooks positions
+
+    //Place knights
+
+    //Place bishops
+
+    //Place queens
+
+    //Place kings
+
+    board = newBoard;
+  }
+  
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       body: GridView.builder(
         itemCount: 8 *8,
         physics: const NeverScrollableScrollPhysics(),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
           itemBuilder: (context, index) {
-            int x = index ~/ 8; // gives us row
-            int y = index % 8; //gives us column
 
-            //checkered pattern
-            bool isWhite = (x+y)%2 == 0;
-            return Square(isWhite: isWhite);
-          }
+          // row and column of this square
+            int row = index ~/ 8; // gives us row
+            int col = index % 8; //gives us column
+
+            return Square(
+                isWhite: isWhite(index),
+                piece: board[row][col],
+            );
+
+          },
       ),
     );
   }
